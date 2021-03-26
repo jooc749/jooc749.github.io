e91ea87262512d0c96cab489c75c90045d2e58ab
jooc749 <jooc749@newschool.edu>
2021-03-25 21:59:06 -0700

    Lab Landing Page

---
+++ script.js
@@ -0,0 +1,57 @@
+//these lines of js are for lab links//
+
+//when clicking class 'a1', alert will display "text" and then go to link//
+
+document.getElementsByClassName('a1')[0].onclick = function(){
+    alert("Assignment 1: Skills Test");
+}
+
+//when clicking class 'a2', alert will display "text" and then go to link//
+
+document.getElementsByClassName('a2')[0].onclick = function(){
+    alert("Assignment 2: Digital Zine");
+}
+
+//when clicking class 'e1', alert will display "text" and then go to link//
+
+document.getElementsByClassName('e1')[0].onclick = function(){
+    alert("Exercise 1: Embedded Content");
+}
+
+//when clicking class 'e2', alert will display "text" and then go to link//
+
+document.getElementsByClassName('e2')[0].onclick = function(){
+    alert("Exercise 2: Translating Printed Media to Web Format");
+}
+
+//when clicking class 'e3', alert will display "text" and then go to link//
+
+document.getElementsByClassName('e3')[0].onclick = function(){
+    alert("Exercise 3: Multimedia Story");
+}
+
+//when clicking class 'e4', alert will display "text" and then go to link//
+
+document.getElementsByClassName('e4')[0].onclick = function(){
+    alert("Exercise 4: Interactive experience for different devices and orientations");
+}
+
+//these lines of js are for studio links//
+
+//when clicking class 'p1', alert will display "text" and then go to link//
+
+document.getElementsByClassName('p1')[0].onclick = function(){
+    alert("Project 1: Cross-Platform Storytelling");
+}
+
+//when clicking class 'p2', alert will display "text" and then go to link//
+
+document.getElementsByClassName('p2')[0].onclick = function(){
+    alert("Project 2: Stories As Networks");
+}
+
+//when clicking class 'p3', alert will display "text" and then go to link//
+
+document.getElementsByClassName('p3')[0].onclick = function(){
+    alert("Project 3: Elastic Collections COMING SOON");
+}
