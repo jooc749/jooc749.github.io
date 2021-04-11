f23618969bef98250199a4256bee73081d89f78b
jooc749 <jooc749@newschool.edu>
2021-04-10 23:10:15 -0700

    K

--- Project3/sidebar.js
+++ Project3/sidebar.js
@@ -1,11 +1,11 @@
 
 function openNav(){
-    document.getElementsByClassName("thesidebar").style.width= "250px";
-    // document.getElementByClassName("main".style.marginLeft= "250px";
+    document.getElementById("thesidebar").style.width= "250px";
+    document.getElementById("main".style.marginLeft)="250px";
 }
 
 function closeNav(){
-    document.getElementByClassName("thesidebar").style.width="0";
-    document.getElementByClassName("main").style.marginLeft="0";
+    document.getElementById("thesidebar").style.width="0";
+    document.getElementById("main").style.marginLeft="0";
 }
 
